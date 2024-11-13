fe92cd0986f1b782a9dc72b57b80e1c6ab79f6f8
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-13 03:55:57 +0000

    adding filter by author

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,16 +4,20 @@ from relationship_app.models import Author, Book, Library, Librarian
  
 def get_books_by_author(author_name): 
     try: 
+        #author_name = "George Orwell"
         author = Author.objects.get(name=author_name) 
         books = author.books.all() 
-        return books 
+        books_by_author = Book.objects.filter(author=author)
+        for book in books_by_author:
+            print(book.title)
+        return books_by_author 
     except Author.DoesNotExist: 
         return f"Author '{author_name}' does not exist." 
     
     # Query 2: List all books in a library 
     
 def get_books_in_library(library_name): 
-    try: 
+    try:
         library = Library.objects.get(name=library_name) 
         books = library.books.all() 
         return books 
