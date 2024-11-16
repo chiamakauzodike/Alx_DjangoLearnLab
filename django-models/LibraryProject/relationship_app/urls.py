83f151d0c2d3e88ce063ced92b91c76371c7a5c9
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-16 17:18:12 +0000

    adding templates for urls mapping views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
+from .views import (
+    LibraryDetailView,
+    list_books,
+    relationship
+)
 
 urlpatterns = [
+    # function based view for listing books
+    path('books/', list_books, name='list-books'),
+    # class based view for displaying library details
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path("", views.relationship, name="relationship"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author, Book, Library, Librarian
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+"""class based view displaying library details and its book"""
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_dtail.html'
+    context_object_name = 'library'
 
 def relationship(request):
     return HttpResponse("Hello, world. You're at the relationship_app with various bookshelf and other connection to Author Library and Librarian class.")
