eb4f5d80a6581b0dac4112d18f4f47fdeaa74133
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-17 23:47:58 +0000

    adding the user profile function

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,6 +9,9 @@ from .views import register
 from views.register import registers
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 
 urlpatterns = [
     # function based view for listing books
@@ -17,6 +20,9 @@ urlpatterns = [
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logot.html'), name='logout'),                                 
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logot.html'), name='logout'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarain/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='memeber_view'),                               
     #path("", views.relationship, name="relationship"),
 ]
