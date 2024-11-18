67e8996577a2de2c0db4c3d6993bfba26ec84af9
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-18 02:47:14 +0000

    adding permission with authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.generics import ListAPIView
 #from rest_framework.generics.ListAPIView import ListAPIView
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
-class BookList(rest_framework.generics.ListAPIView): #pass the test
+class BookList(generics.ListAPIView): #pass the test
 #class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(rest_framework.viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
 #class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
