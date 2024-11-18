4ee0671758df6ee71ab79a0b88218e474cdc6362
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-18 00:10:46 +0000

    craeting vies that user passes test update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -45,15 +45,15 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request)
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request)
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request)
 
 
 def relationship(request):
