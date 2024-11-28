81a55edbf91ace4d76abb8fe0dcb60805123efb5
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-11-28 02:21:17 +0000

    adding advanced API project setup update

--- advanced_api_project/advanced_api_project/api/models.py
+++ advanced_api_project/advanced_api_project/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import date
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE) #one-to-many relationship
+
+    def clean(self):
+        # Customising validation to make sure publication year is not in the future.
+        if self.publication_year > date.today().year:
+            raise ValidationError("Publication year should be in the past and not future")
+        
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
+  
