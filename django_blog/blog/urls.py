d84d81c9a5ffdb406022d61c5d1ebd8ae4a69507
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-12-08 23:00:18 +0000

    adding the comment model update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/new/', add_comment, name='add-comment'),
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
