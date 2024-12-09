25231f3057571802b34bf0ac626a6afff2f77891
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-12-09 02:13:59 +0000

    updated django-blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,8 +6,8 @@ from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     tag = TaggableManager()
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '5432',
+        'CONN_MAX_AGE': 0,
     }
 }
 
