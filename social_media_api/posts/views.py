49cc139af7d2f287cfb4d8d556b8c8f7f4837c66
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-12-15 23:11:08 +0000

    adding the new app notifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -9,6 +10,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # My app
     'accounts',
     'posts',
+    'notifications',
 ]
 
 MIDDLEWARE = [
