47628a242270cee4289901354fde14ab41b34234
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2024-12-23 00:31:41 +0000

    adding the account serializer update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,25 +3,33 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 # Get the custom user model
-User = get_user_model()
+CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     # Define a password field explicitly to ensure proper write-only behavior
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    bio = serializers.CharField(required=False, allow_blank=True, max_length=255)
+    profile_picture = serializers.ImageField(required=False, allow_null=True)
+    password = serializers.CharField(write_only=True)
+
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
         read_only_fields = ['followers']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
         # Remove the password from validated_data and use `create_user` to hash it properly
         password = validated_data.pop('password')
         user = get_user_model().objects.create_user(**validated_data)
-        user.set_password(password)  # Hash the password
-        user.save()
-
-        # Create a token for the new user
-        Token.objects.create(user=user)
-
+        if password:
+            user.set_password(password)  # Hash the password
+            user.save()
         return user
+
+        #  Serializer for managing user follow actions. Create a token for the new user
+class FollowSerializer(serializers.Serializer):
+    follower = serializers.CharField(read_only=True)
+    following = serializers.CharField()
